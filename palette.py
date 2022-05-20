23f3e5d779e6dbe93675978fad7e7aeef659daaa
Phil Howard <phil@gadgetoid.com>
2022-05-20 13:20:04 +0100

    Fix palette generator

--- palette.py
+++ palette.py
@@ -1,10 +1,15 @@
 from matplotlib import cm
 import numpy
+import sys
+
+
+palette = sys.argv[1] if len(sys.argv) > 1 else "plasma"
+
 
 def to_int16(c):
     r, g, b = c
-    return ((r & 0b11111000) << 8) | ((g & 0b11111100) << 3) | ((b & 0b1111100) >> 3)
-
+    packed = ((r & 0b11111000) << 8) | ((g & 0b11111100) << 3) | ((b & 0b11111000) >> 3)
+    return ((packed & 0xff) << 8) | (packed >> 8)
 
 def get_palette(name):
     cmap = cm.get_cmap(name, 256)
@@ -18,10 +23,11 @@ def get_palette(name):
     arr = arr.reshape((16, 16, 4))
     arr = arr[:, :, 0:3]
     arr = arr.reshape((16 * 16, 3))
+    print(arr)
     return [to_int16(x) for x in arr]
 
 
-pal = ["0x{:04x}".format(x) for x in get_palette("plasma")]
+pal = ["0x{:04x}".format(x) for x in get_palette(palette)]
 
 for x in range(0, 256, 16):
     pals = pal[x:x+16]
